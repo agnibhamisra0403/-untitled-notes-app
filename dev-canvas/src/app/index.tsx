a037e0a6acc6408f041c96f7c6584896a688f1a7
Agnibha Misra <agnibhamisra0403@gmail.com>
2026-05-16 17:45:12 -0400

    comepleted User stories 1 to 5

--- dev-canvas/src/app/index.tsx
+++ dev-canvas/src/app/index.tsx
@@ -1,15 +1,34 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, GestureResponderEvent } from 'react-native';
 import { Canvas, Circle } from '@shopify/react-native-skia';
+import { useCanvasState } from '../hooks/useCanvasState';
 
 export default function App() {
+  const { 
+    currentLine, 
+    completedLines, 
+    handleTouchStart, 
+    handleTouchMove, 
+    handleTouchEnd 
+  } = useCanvasState();
+
+  console.log(
+    `Active Points: ${currentLine?.points.length || 0} | Total Lines Saved: ${completedLines.length}\n`    
+  );
+
   return (
     <View style={styles.container}>
       <Canvas style={styles.canvas}>
         <Circle cx={200} cy={300} r={50} color="cyan" />
       </Canvas>
+      <View 
+        style={StyleSheet.absoluteFill}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      />
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
---
+++ dev-canvas/src/hooks/useCanvasState.ts
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { GestureResponderEvent } from 'react-native';
+import { Line, Point } from '../types/canvas';
+
+export const useCanvasState = () => {
+    const [currentLine, setCurrentLine] = useState<Line | null>(null);
+    const [completedLines, setCompletedLines] = useState<Line[]>([]);
+
+
+    const handleTouchStart = (event: GestureResponderEvent) => {
+        const x = event.nativeEvent.locationX;
+        const y = event.nativeEvent.locationY;
+
+        const point: Point = { x, y };
+
+        const newLine: Line = {
+            points: [point],
+            color: '#00FFFF',
+            width: 5,
+        };
+
+        setCurrentLine(newLine);
+    };
+
+    const handleTouchMove = (event: GestureResponderEvent) => {
+        if (!currentLine) return;
+
+        const x = event.nativeEvent.locationX;
+        const y = event.nativeEvent.locationY;
+        
+        const newPoint: Point = {x, y};
+        const updatedLine: Line = {...currentLine, points: [...currentLine.points, newPoint]};
+        
+        setCurrentLine(updatedLine);
+
+    };
+    
+    const handleTouchEnd = () => {
+        if (!currentLine) return;
+        setCompletedLines([...completedLines, currentLine]);
+        setCurrentLine(null);
+    };
+
+    return {
+        currentLine,
+        completedLines,
+        setCompletedLines,
+        handleTouchStart,
+        handleTouchMove,
+        handleTouchEnd,
+    };
+};
