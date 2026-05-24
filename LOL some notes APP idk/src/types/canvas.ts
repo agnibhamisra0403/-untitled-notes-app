4e39978d4145216ff4d6725fdf3490fbc73b27e9
Prisha Desai <prishdesai05@gmail.com>
2026-05-24 16:27:56 -0400

    start epic 11

--- LOL some notes APP idk/src/types/canvas.ts
+++ LOL some notes APP idk/src/types/canvas.ts
@@ -1,33 +1,35 @@
-import { SkImage } from '@shopify/react-native-skia';
+import { SkImage } from "@shopify/react-native-skia";
 
 export interface ImageAsset {
-    id: string;
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    rotation: number;
-    scale: number;
-    image: SkImage;
+  id: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rotation: number;
+  scale: number;
+  image: SkImage;
 }
 
 export interface Point {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
-
 
 export interface Line {
-    points: Point[];
-    color: string;
-    width: number;
-    bounds?: BoundingBox;
+  points: Point[];
+  color: string;
+  width: number;
+  bounds?: BoundingBox;
 }
 
-
 export interface BoundingBox {
-    minX: number;
-    maxX: number;
-    minY: number;
-    maxY: number;
+  minX: number;
+  maxX: number;
+  minY: number;
+  maxY: number;
+}
+
+export interface CodeBlock {
+  id: string;
 }
